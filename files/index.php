6837c9513ee9428570efdf70d9cf197a15bd1de1
anmuhina <112845722+anmuhina@users.noreply.github.com>
2023-02-25 21:11:33 +0300

    Update index.php

--- files/index.php
+++ files/index.php
@@ -1,14 +1,6 @@
-/*<?php
+<?php
 header('Content-Type: text/html; charset=UTF-8');
   if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   print_r($_POST);
   }
-?>*/
-
-<?php
-$comment = "не определен";
-if(isset($_POST["comment"])){
-    $comment = $_POST["comment"];
-}
-echo "Comment: $comment";
 ?>
