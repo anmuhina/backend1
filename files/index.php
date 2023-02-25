046ac9a0aef9ab80faf14f0407a895adceba2b1e
anmuhina <112845722+anmuhina@users.noreply.github.com>
2023-02-25 15:15:22 +0300

    Update index.php

--- files/index.php
+++ files/index.php
@@ -8,7 +8,7 @@ header('Content-Type: text/html; charset=UTF-8');
 <?php
 
 // Отправляем правильную кодировку.
-header('HTTP/1.1 404 Not Found');
+//header('HTTP/1.1 404 Not Found');
 header('Content-Type: text/html; charset=UTF-8');
 
 // Выводим все полученные через POST параметры.
